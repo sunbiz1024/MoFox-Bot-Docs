b5fd15d5f74db7812da41499600f72e5182f971a
subiz <1656525855@qq.com>
2025-09-21 07:57:18 +0800

    Revert "fix：修复编译问题"
    This reverts commit 3c959d04e1c7727676fcb4406cc40eeb8f64a29d.

--- .vitepress/theme/ReadingTime.vue
+++ .vitepress/theme/ReadingTime.vue
@@ -18,9 +18,6 @@ const wordCountText = ref('');
 import { nextTick } from 'vue';
 
 const calculateReadingTime = () => {
-  if (typeof window === 'undefined') {
-    return;
-  }
   nextTick(() => {
     const content = document.querySelector('.vp-doc');
     if (content) {
